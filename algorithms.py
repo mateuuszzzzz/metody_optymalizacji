7ddfcb4dd2f6419a58daed985f092b764f98465d
Mateusz Rajski <mateusz.rajski@swmansion.com>
2024-04-21 16:12:11 +0200

    Plot external method algorithm steps

--- algorithms.py
+++ algorithms.py
@@ -22,25 +22,25 @@ def external_penalty_function_method(
     def penalty(x, current_rho):
         inequality_penalties = np.sum(
             [np.power(np.where(constraint(x) < 0, 0, constraint(x)), 2) for constraint in inequality_constraints],
-            axis=-1
+            axis=0
         )
 
         equality_penalties = np.sum(
             [np.power(np.abs(constraint(x)), 2) for constraint in equality_constraints],
-            axis=-1
+            axis=0
         )
 
         return current_rho * (equality_penalties + inequality_penalties)
     
     prev_x = None
     prev_rho = None
-
     current_step = 1
 
     while penalty(x, 1) >= epsilon:
 
-        opt_steps = []
+        opt_steps = [np.array(x)]
         # Minimize F for current x and rho
+
         opt_data = minimize(
             method='Powell',
             fun=lambda x: f(x) + penalty(x, rho),
@@ -54,24 +54,23 @@ def external_penalty_function_method(
             print(opt_data.message)
             break
 
-        rho = c*rho
-        x = opt_data.x
-
-        # Between every 2 steps algorithm should hold those assumptions
-        if prev_rho is not None:
-            assert penalty(prev_x, 1) >= penalty(x, 1)
-            assert f(prev_x) + penalty(prev_x, prev_rho) <= f(x) + penalty(x, rho)
-            assert f(prev_x) <= f(x)
-
         if callback:
             callback(
                 f,
                 penalty,
                 rho,
-                opt_steps,
+                np.hstack(opt_steps).flatten(),
                 current_step,
-            )
-            
+        )
+
+        rho = c*rho
+        x = opt_data.x
+
+        # Between every consequtive steps algorithm should hold those assumptions
+        if prev_rho is not None:
+            assert penalty(prev_x, 1) >= penalty(x, 1)
+            assert f(prev_x) + penalty(prev_x, prev_rho) <= f(x) + penalty(x, rho)
+            assert f(prev_x) <= f(x)
 
         prev_x = x
         prev_rho = rho
@@ -89,9 +88,7 @@ if __name__ == '__main__':
         lambda x: x-3 # x-3 <= 0
     ]
 
-    equality_constraints_list = [
-        lambda x: x+2 # x+2 == 0
-    ]
+    equality_constraints_list = [ ]
 
     INIT_RHO = 1
     INIT_X = 9
